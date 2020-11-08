8c5f5e678d44763e9bf7e752f12bcdb23176d9fa
Vsevolod Bazhan <vsevozhan@yandex.ru>
2020-11-08 21:03:01 +1000

    Implement `CopyMoviesToSQLTable`

--- src/tasks.py
+++ src/tasks.py
@@ -2,7 +2,10 @@ import csv
 import os
 
 import luigi
+import pandas as pd
 from luigi.contrib.sqla import CopyToTable
+from luigi.util import requires
+from sqlalchemy import Float, Integer, String
 
 from .genres import Genre
 from .movies import get_top_movies
@@ -29,3 +32,34 @@ class LoadTopMovies(luigi.Task):
 
     def output(self):
         return luigi.LocalTarget(os.path.join(self.directory_name, self.file_name))
+
+
+@requires(LoadTopMovies)
+class CopyMoviesToSQLTable(CopyToTable):
+    table = "TopMovie"
+    connection_string = f"sqlite:///top_movies.db"
+    columns = [
+        (["id", Integer()], {"primary_key": True}),
+        (["title", String()], {}),
+        (["genre", String()], {}),
+        (["rating", Float()], {}),
+        (["year", Integer()], {}),
+        (["runtime", Integer()], {}),
+        (["votes", Integer()], {}),
+        (["metascore", Integer()], {}),
+        (["certificate", String()], {}),
+        (["gross", Float()], {}),
+    ]
+
+    def rows(self):
+        with self.input().open("r") as input:
+            df = pd.read_csv(input)
+            df.index += 1
+            df.index.name = "id"
+            splitted = df.to_dict(orient="split")
+
+            rows = []
+            for id, values in zip(splitted["index"], splitted["data"]):
+                rows.append([id, *values])
+
+            return rows
